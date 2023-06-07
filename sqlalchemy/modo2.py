b67e82ace3b0ca940fada2592966b7716080d5db
keynerrex <keyneroliveros24@gmail.com>
2023-06-07 14:30:51 -0500

    Pruebas sqlalchemy

--- sqlalchemy/modo2.py
+++ sqlalchemy/modo2.py
@@ -1,10 +1,15 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, DateTime, create_engine, text
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import (Column, Integer, String, DateTime,
+                        create_engine, text, CheckConstraint,
+                        ForeignKey, select, func, and_, select)
 
 engine = create_engine(
     'mysql://keynerrex:keynerdel2015@localhost:3307/sqlalchemy')
 Base = declarative_base()
+
+Session = sessionmaker(engine)
+session = Session()
 
 
 class User(Base):
@@ -31,6 +36,29 @@ class Correo(Base):
 
     def __str__(self):
         return self.correo
+
+
+class Padre(Base):
+    __tablename__ = 'Padre'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(30), server_default='No tiene')
+    apellido = Column(String(30), server_default='No tiene')
+    edad = Column(Integer, server_default=text('0'))
+    __table_args__ = (
+        CheckConstraint('edad >= 0 AND edad <= 99', name='chk_edad_range'),
+    )
+    hijos = relationship("Hijo", back_populates="padre")
+    creado = Column(DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+
+
+class Hijo(Base):
+    __tablename__ = 'Hijo'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(30), server_default='No tiene')
+    apellido = Column(String(30), server_default='No tiene')
+    padre_id = Column(Integer, ForeignKey('Padre.id'))
+    creado = Column(DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+    padre = relationship("Padre", back_populates="hijos")
 
 
 def consultas2():
@@ -64,6 +92,7 @@ def filtros2():
 def filtros3():
     # Consultas Unicas( first(devuelve None),one(lanza excepcion) )
     """SELECT * FROM users WHERE id = 50 LIMIT 1"""
+
     user = session.query(User).filter(User.id == 50).first()
 
     if user is not None:
@@ -73,31 +102,44 @@ def filtros3():
 
 
 def actualizar():
-    """SELECT * FROM users WHERE id = 50"""
+    """SELECT * FROM users WHERE id = 1011"""
 
-    user = session.query(User).filter(User.id == 5).first()
-    user.usuario = 'usernuevo50'
-    user.set_correo('usernuevo5000@gmail.com')
+    try:
+        user = session.query(User).filter(User.id == 1011).first()
+        user.usuario = 'usernuevo50'
+        user.set_correo('usernuevo5000@gmail.com')
 
-    session.add(user)
-    session.commit()
-    print(user)
+        session.add(user)
+        session.commit()
+        print(user)
+
+    except Exception as e:
+        print(f"Este usuario no existe, no se pudo actualizar")
+
+    finally:
+        session.rollback()
+        session.close()
 
 
 def actualizar2():
     # 2
-    """UPDATE user SET usuario = 'usernuevo156',correo = 'emailnuevo156@gmail.com'
-    WHERE (id = '51');"""
+    """UPDATE user SET usuario = 'usernuevo1501',correo = 'keyner1501@gmail.com'
+    WHERE (id = '1501');"""
 
-    session.query(User).filter(User.id == 156).update(
-        {
-            User.usuario: 'usernuevo157',
-            User.correo: 'keyner@gmail.com'.lower()
-        }
-    )
+    try:
+        session.query(User).filter(User.id == 1501).update(
+            {
+                User.usuario: 'usernuevo1501',
+                User.correo: 'keyner1501@gmail.com'.lower()
+            }
+        )
 
-    session.commit()
-    print(session.query(User.usuario, User.correo).filter(User.id == 156).first())
+        session.commit()
+        print(session.query(User.id, User.usuario, User.correo).filter(
+            User.id == 1501).first())
+
+    except Exception as e:
+        print("error: {e}")
 
 
 def eliminar():
@@ -116,25 +158,56 @@ def eliminar():
         session.close()
 
 
-Session = sessionmaker(engine)
-session = Session()
+def consulta_relacionada1():
+    """SELECT h.id, h.nombre, h.apellido, CONCAT(p.nombre,' ',p.apellido) AS Padre 
+    FROM hijo AS h
+    INNER JOIN padre AS p
+    ON h.padre_id = p.id;"""
 
-__name__ = 'actualizar_1_2'
+    query = select([
+        Hijo.id,
+        Hijo.nombre,
+        Hijo.apellido,
+        func.concat(Padre.nombre, ' ', Padre.apellido).label('Padre')
+    ]).join(Padre, Hijo.padre_id == Padre.id)
+
+    print(query)
+
+
+def consulta_relacionada2() -> list:
+    """SELECT h.id, h.nombre, h.apellido,
+    CONCAT(p.nombre,' ',p.apellido) AS Padre, h.creado
+    FROM hijo AS h
+    INNER JOIN padre AS p
+    ON h.padre_id = p.id
+    WHERE h.apellido = p.apellido;"""
+
+    query = session.query(
+        Hijo.id, Hijo.nombre, Hijo.apellido,
+        func.concat(Padre.nombre, ' ', Padre.apellido), Hijo.creado).\
+        join(Padre, Hijo.padre_id == Padre.id).filter(
+            Hijo.apellido == Padre.apellido).all()
+
+    resultados = []
+    for hijo in query:
+        resultados.append(
+            (hijo[0], hijo[1], hijo[2], hijo[3], hijo[4]))
+
+    return resultados
+
+
+__name__ = 'consultas_relacionadas'
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     usr = 'keynerrex'
-    crr = 'keyner@gamil.com'
+    crr = 'keyner@gmil.com'
 
     try:
-        """INSERT INTO users (username,correo)
-        VALUES('usuario','example@gmail.com')"""
-
-        user1 = User(usuario=usr, correo=crr)
-        session.add(user1)
-        session.commit()
-        print(f"Inserccion correcta: {user1}")
+        query = session.query(Hijo).with_entities(
+            Hijo.id, Hijo.nombre, Hijo.apellido, Padre.nombre).all()
+        print(query)
 
     except Exception as err:
         session.rollback()
@@ -156,7 +229,13 @@ elif __name__ == 'filtros3':
     filtros3()
 
 elif __name__ == 'actualizar_1_2':
-    actualizar()
+    actualizar2()
 
 elif __name__ == 'eliminar':
     eliminar()
+
+elif __name__ == 'consultas_relacionadas':
+    resultados = consulta_relacionada2()
+    for hijo in resultados:
+        print(
+            f"{hijo[0]} --- {hijo[1]} --- {hijo[2]} --- {hijo[3]} --- {hijo[4]}")
