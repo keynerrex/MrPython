e5bb809e4dbf3646a7507486f9731b7730095364
keynerrex <keyneroliveros24@gmail.com>
2023-09-25 11:47:34 -0500

    Gen numbers

--- flask_cod_facilito/config.py
+++ flask_cod_facilito/config.py
@@ -18,8 +18,8 @@ class DevelopmentConfig(Config):
     DB_NAME = 'flask'
     DB_USER = 'keynerrex'
     DB_SERVER = 'localhost'
-    DB_PASS = ''
-    DB_PORT = 3306
+    DB_PASS = 'keynerdel2015'
+    DB_PORT = 3307
     SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PASS}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
