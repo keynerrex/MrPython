5afefb990f82f7dc4ba90cc8fad0f03af2e8b401
keynerrex <keyneroliveros24@gmail.com>
2023-11-14 11:36:22 -0500

    Cambio de base de datos produccion

--- flask_cod_facilito/config/config.py
+++ flask_cod_facilito/config/config.py
@@ -17,8 +17,8 @@ class ProductionConfig(Config):
     DEBUG = True
     DB_NAME = 'flask'
     DB_USER = 'root'
-    DB_SERVER = 'containers-us-west-35.railway.app'
-    DB_PASS = 'kvJdDcbVYE2CaDXWiiA2'
-    DB_PORT = 6043
+    DB_SERVER = '127.0.0.1'
+    DB_PASS = '12345678'
+    DB_PORT = 3307
     SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PASS}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
