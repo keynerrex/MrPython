2c143d2d6c7c91d25d625488c6cb6a847ba3a6f4
keynerrex <keyneroliveros24@gmail.com>
2023-12-11 16:26:47 -0500

    WIP: 1d8661e cambio a w11

--- flask_cod_facilito/routes/users.py
+++ flask_cod_facilito/routes/users.py
@@ -1,7 +1,7 @@
 # routes/users.py
 from sqlalchemy.exc import IntegrityError
 from flask import Blueprint, render_template, request, jsonify
-from utils.decorators.decorators import admin_role_required
+from utils.decorators.decorators import role_required
 from models import db, User, Rol
 
 users_routes = Blueprint('users', __name__)
@@ -15,6 +15,7 @@ def usuarios():
 
 
 @users_routes.route(f'{path_url}usuarios_json')
+@role_required('Administrador')
 def usuarios_json():
     users = User.query.join(Rol, isouter=True).with_entities(
         User.id,
@@ -55,7 +56,7 @@ def registros():
 
 
 @users_routes.route(f'{path_url}usuarios_registrados_json', methods=['GET'])
-@admin_role_required
+@role_required('Administrador')
 def users_registers():
     page = request.args.get('page', 1, type=int)
     search_term = request.args.get('search', '', type=str)
@@ -67,7 +68,7 @@ def users_registers():
         User.status,
         User.create_date,
         Rol.rol
-    ).join(Rol, isouter=True).filter(
+    ).outerjoin(Rol).filter(
         User.username.ilike(
             f"%{search_term}%")).order_by(User.id).paginate(
         page=page, per_page=5, error_out=False)
@@ -79,8 +80,8 @@ def users_registers():
             "user_id": user.id,
             "username": user.username,
             "email": user.email,
-            "status": user.status,
-            "rol": rol,
+            "status": user.status if user.status else 'Error de estado',
+            "rol": user.rol if user.rol else 'Sin rol asignado',
             "create_date": user.create_date.strftime("%d de %B del %Y")
         })
 
@@ -91,7 +92,7 @@ def users_registers():
 
 
 @users_routes.route(f'{path_url}usuarios-registrados', methods=['GET'])
-@admin_role_required
+@role_required()
 def show_users_registers():
     rols = Rol.query.order_by(Rol.rol).all()
 
--- flask_cod_facilito/utils/decorators/decorators.py
+++ flask_cod_facilito/utils/decorators/decorators.py
@@ -1,23 +1,53 @@
+from models import User
 from flask import session, redirect, url_for, render_template
 from functools import wraps
+# Asegúrate de importar tus modelos adecuadamente
 from models import db, User, Rol
 
 
-def admin_role_required(adm_user):
-    @wraps(adm_user)
-    def decorated_rol(*args, **kwargs):
+def admin_role_required(view):
+    @wraps(view)
+    def decorated_view(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('home.login'))
+
         username = session.get('username')
-        rol_administrador = db.session.query(User.username, Rol.rol)\
-            .join(Rol).filter(
-                User.username == username,
-                Rol.rol == 'Administrador').first()
+        user = User.query.filter_by(username=username).first()
 
-        if not rol_administrador:
-            return inauthorized()
+        if not user or user.status != 1:
+            return render_template('inauthorized.html', code_err=401), 401
 
-        return adm_user(*args, **kwargs)
+        # Verificar si el usuario tiene el rol de Administrador
+        if user.rol.rol != 'Administrador':
+            return render_template('inauthorized.html', code_err=401), 401
 
-    return decorated_rol
+        return view(*args, **kwargs)
+
+    return decorated_view
+
+
+def role_required(*allowed_roles):
+    def decorator(view):
+        @wraps(view)
+        def decorated_view(*args, **kwargs):
+            if 'username' not in session:
+                return redirect(url_for('home.login'))
+
+            username = session.get('username')
+            user = User.query.filter_by(username=username).first()
+
+            if not user or user.status != 1 or not user.rol:
+                return render_template('inauthorized.html', code_err=401), 401
+
+            # Permitir el acceso si no se proporcionan roles específicos o si el usuario tiene uno de los roles permitidos
+            if not allowed_roles or user.rol.rol in allowed_roles:
+                return view(*args, **kwargs)
+
+            return render_template('inauthorized.html', code_err=401), 401
+
+        return decorated_view
+
+    return decorator
 
 
 def inauthorized():
