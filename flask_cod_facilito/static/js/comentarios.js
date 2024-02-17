306684937d01f6b6ba632430fd4cfaa5423b734f
keynerrex <keyneroliveros24@gmail.com>
2024-02-17 10:21:19 -0500

    Arreglo bug - No aparecen comentarios en la tabla

--- flask_cod_facilito/static/js/comentarios.js
+++ flask_cod_facilito/static/js/comentarios.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       loadingContainer.style.display = "none";
       // Mostrar la tabla
       tableBody.style.display = "table-row-group";
-    }, 1000);
+    }, 500);
   }
 
   function fetchData(searchTerm) {
@@ -62,17 +62,15 @@ document.addEventListener("DOMContentLoaded", function () {
     .getElementById("search-input")
     .addEventListener("input", function () {
       const searchTerm = this.value.toLowerCase();
-      debounce(() => fetchAndDisplayData(searchTerm), 50); // Llama a fetchAndDisplayData después de un retraso de 50 ms
+      debounce(() => fetchData(searchTerm), 50); // Llama a fetchAndDisplayData después de un retraso de 50 ms
     });
 
-  document
-    .getElementById("report-button")
-    .addEventListener("click", function () {
-      for (let i = 0; i < 10; i++) {
-        console.log("Generando reporte...");
-      }
-    });
+  document.getElementById("report-button").addEventListener("click", () => {
+    for (let i = 0; i < 10; i++) {
+      console.log("Generando reporte...");
+    }
+  });
 
   // Llamar a la función al cargar la página
-  fetchData();
+  fetchData("");
 });
